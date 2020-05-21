5c45fe627b53b9ead8c8fdcd4f74138faed23e40
brworkit <brworkit@gmail.com>
2020-05-21 01:12:13 -0300

    using my own package flutter

--- pokeapp/lib/main.dart
+++ pokeapp/lib/main.dart
@@ -6,12 +6,10 @@ void main() {
   runApp(App());
 
   // print("green".color());
-  
   // Colors.primaries.forEach((color) {
   //   print("color: " + color.value.toString());
   // });
   // for (Color color of Colors.primaries) {
-
   // }
 }
 
--- pokeapp/lib/pages/pokemon_list_view.dart
+++ pokeapp/lib/pages/pokemon_list_view.dart
@@ -1,47 +1,36 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-import 'package:pokeapp/extensions/string_to_color.dart';
+import 'package:strcolor/strcolor.dart';
 
 class PokemonListView extends StatelessWidget {
   const PokemonListView(this.items) : super();
-  
+
   final List<Map<String, dynamic>> items;
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
         (context, index) {
           var item = this.items[index];
-          var types = createTypesBadges(item);
-          return Card(
-              child: ListTile(
-            leading: Image.network(
-              item["image"],
-              fit: BoxFit.fill,
-            ),
-            title: Text(item["name"].toUpperCase(),
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            subtitle: Container(
-              child: types,
-            ),
-            trailing: Badge(
-                badgeColor: item["color"].toString().color(),
-                badgeContent: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    item["id"].toString(),
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                )),
-          ));
+          return PokemonPreview(item: item);
         },
         // Builds 1000 ListTiles
         childCount: items.length,
       ),
+    );
+  }
+}
+
+class PokemonTypesView extends StatelessWidget {
+  const PokemonTypesView({Key key, @required this.item}) : super(key: key);
+
+  final Map<String, dynamic> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: _createChildren(item["types"]),
     );
   }
 
@@ -58,11 +47,38 @@ class PokemonListView extends StatelessWidget {
           ));
     });
   }
+}
 
+class PokemonPreview extends StatelessWidget {
+  const PokemonPreview({Key key, @required this.item}) : super(key: key);
 
-  createTypesBadges(Map<String, dynamic> item) {
-    return Row(
-      children: _createChildren(item["types"]),
-    );
+  final Map<String, dynamic> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Image.network(
+        item["image"],
+        fit: BoxFit.fill,
+      ),
+      title: Text(item["name"].toUpperCase(),
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+      subtitle: Container(
+        child: PokemonTypesView(item: item),
+      ),
+      trailing: Badge(
+          badgeColor: item["color"].toString().color(),
+          badgeContent: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              item["id"].toString(),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+          )),
+    ));
   }
 }
