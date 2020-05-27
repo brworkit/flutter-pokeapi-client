aede5a7c0422c3e8331302af8f468a83d15c6317
brworkit <brworkit@gmail.com>
2020-05-26 23:42:22 -0300

    TEST NEW BADGER

--- pokeapp/lib/widgets/pokemon_overview_types_badges.dart
+++ pokeapp/lib/widgets/pokemon_overview_types_badges.dart
@@ -3,36 +3,67 @@ import 'package:flutter/material.dart';
 import 'package:strcolor/strcolor.dart';
 
 class PokemonOverviewTypesBadges extends StatelessWidget {
-  const PokemonOverviewTypesBadges({Key key, @required this.item}) : super(key: key);
+  const PokemonOverviewTypesBadges({Key key, @required this.item})
+      : super(key: key);
 
   final Map<String, dynamic> item;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      // children: _createChildren(item["types"].reversed.toList()),
+    return Row(      
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: _createChildren(item["types"]),
     );
   }
 
   List<Widget> _createChildren(List<Map<String, dynamic>> itemTypes) {
     return List<Widget>.generate(itemTypes.length, (int index) {
+
       var itemType = itemTypes[index];
-      Color badgeColor = itemType["color"]["hex"].toString().color();
+      var colorHex = itemType["color"]["hex"].toString();
+      Color backgroundColor = colorHex.color().withOpacity(0.6);
+      
       return Container(
-        alignment: Alignment.topRight,
-        child: Badge(
-            elevation: 5,
-            shape: BadgeShape.square,
-            badgeColor: badgeColor,
-            badgeContent: Text(
-              itemType["name"],
-              style: TextStyle(
-                  color: badgeColor.contrast(),
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold),
-            )),
+        alignment: Alignment.topLeft,
+        child: HexaRectangleBadge(
+          child: Text(
+            itemType["name"],
+            style: TextStyle(
+                color: backgroundColor.contrast(),
+                fontSize: 12,
+                fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: backgroundColor,
+        ),
       );
     });
+  }
+}
+
+class HexaRectangleBadge extends StatelessWidget {
+  const HexaRectangleBadge({
+    Key key,
+    @required this.backgroundColor,
+    @required this.child,
+  }) : super(key: key);
+
+  final Color backgroundColor;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+        clipBehavior: Clip.antiAlias,
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Container(
+            width: 50,
+            height: 20,
+            color: backgroundColor,
+            child: Center(
+              child: child,
+            ),
+          ),
+        ));
   }
 }
