3da09d0f7e73a41cd8b90f3c88b303c8ca171a7d
brworkit <brworkit@gmail.com>
2020-06-06 12:42:32 -0300

    loading

--- pokeapp/lib/widgets/pokemons_grid.dart
+++ pokeapp/lib/widgets/pokemons_grid.dart
@@ -12,19 +12,15 @@ class PokemonsGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Pokemon> items = Provider.of<Pokemons>(context).items;
 
+    if(items.isEmpty) {
+      return Center(child: CircularProgressIndicator(backgroundColor: Colors.red,));
+    }
+
     return GridView.builder(
       padding: EdgeInsets.all(8.0),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return PokemonOverviewItem(item: items[index], index: index,);
-        // return GestureDetector(
-        //     onTap: () {
-        //       print("onTap");
-
-        //       Navigator.of(context).push(MaterialPageRoute(
-        //           builder: (context) => PokemonDetailPage(index)));
-        //     },
-        //     child: PokemonOverviewItem(item: items[index]));
+        return PokemonOverviewItem(item: items[index], index: index,);        
       },
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
